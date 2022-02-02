ac52c77718e71f7acd1981f4ebcac2bc2fa5cfb3
aleshya-barbour <aleshya.ab@gmail.com>
2022-02-02 16:33:20 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Clock from './components/Clock';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>
+        What's good nigga!
+        <Clock />
+      </h2>
+    </div>
+  );
+}
+
+export default App;
